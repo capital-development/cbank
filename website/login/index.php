2cd3e25459f38064c340fa52687bf7b959ce7872
Capital Nerd <94248454+capital-development@users.noreply.github.com>
2022-08-29 18:38:02 +0200

    Create index.php

---
+++ website/login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>C-Bank</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body class="text-center">
+    <main class="form-signin">
+        <form>
+          <h1 class="h3 mb-3 fw-normal">Bitte melde dich an</h1>
+          <div class="form-floating">
+            <input type="text" class="form-control" id="floatingInput" placeholder="Username" name="username" required>
+            <label for="floatingInput">Username</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="passw" required>
+            <label for="floatingPassword">Passwort</label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Anmelden</button>
+          <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
+        </form>
+      </main>
+</body>
+</html>
